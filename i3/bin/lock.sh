85cfae300a85cc69ddfbf9b9204eb60f96a99e6e
codem0nk <acodem0nk@gmail.com>
2017-05-08 02:14:11 +0700

    Added functions to see if I could ¯\_(ツ)_/¯

--- i3/bin/lock.sh
+++ i3/bin/lock.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
-# Use scrot to take screenshot of desktop & apply pixelation
+set -e
 
+# Use scrot to take screenshot of desktop & apply pixelation
+function screenshot {
     scrot /tmp/screen.png
     convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+}
 
-# Apply lock image to screenshot
-
+# Apply lock image to pixelated screenshot
+function lockimg {
     convert /tmp/screen.png $HOME/lock-pic.png -gravity center -composite -matte /tmp/screen.png
+}
 
 # Stop playing music
+function stopmusic {
+    exec cmus-remote -s
+}
 
-    exec cmus-remote -s &
+# Call functions to perform desired action
+screenshot && \
+lockimg && \
 
-# Run i3lock script & delete image in /tmp
+# Call i3lock script & delete image in /tmp
+i3lock -u -i /tmp/screen.png
+rm /tmp/screen.png
 
-    i3lock -u -i /tmp/screen.png
-    rm /tmp/screen.png
+# Stopmusic funtion last because it breaks shit ¯\_(ツ)_/¯
+stopmusic
+
+#nailed it
