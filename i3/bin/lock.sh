27f7de21a2ecaee3896276cb86978a02999ee321
codem0nk <acodem0nk@gmail.com>
2017-05-08 01:27:54 +0700

    Tidied-up comments for lock.sh

--- i3/bin/lock.sh
+++ i3/bin/lock.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# Use scrot to take screenshot of desktop
-scrot /tmp/screen.png
+# Use scrot to take screenshot of desktop & apply pixelation
 
-# Apply pixelation to screenshot
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+    scrot /tmp/screen.png
+    convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 
 # Apply lock image to screenshot
-convert /tmp/screen.png $HOME/lock-pic.png -gravity center -composite -matte /tmp/screen.png
 
-# Pause playing music
-exec cmus-remote -u &
+    convert /tmp/screen.png $HOME/lock-pic.png -gravity center -composite -matte /tmp/screen.png
 
-# Run i3lock script
-i3lock -u -i /tmp/screen.png
-rm /tmp/screen.png
+# Stop playing music
+
+    exec cmus-remote -s &
+
+# Run i3lock script & delete image in /tmp
+
+    i3lock -u -i /tmp/screen.png
+    rm /tmp/screen.png
